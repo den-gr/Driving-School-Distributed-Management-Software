a5f6987a22ae2119b9e61d1b44cd518df5d318ef
Denys <denys.grushchak@studio.unibo.it>
2023-04-12 13:58:21 +0200

    change jvm version to 1.7.10. Add cucumber gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.20"
+    kotlin("jvm") version "1.7.10"
     id("io.vertx.vertx-plugin") version "1.4.0"
     application
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("se.thinkcode.cucumber-runner") version "0.0.11"
 }
+
 vertx.mainVerticle="dsdms.client.Client"
 
 repositories {
@@ -21,7 +24,7 @@ subprojects{
 
 }
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "16"
 }
 
 dependencies {
@@ -31,7 +34,7 @@ dependencies {
     testImplementation("io.cucumber:cucumber-java:7.11.2")
     testImplementation("io.cucumber:cucumber-junit:7.11.2")
     testImplementation("io.cucumber:cucumber-java8:7.11.2")
-    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.2")
 
 }
 
@@ -40,8 +43,20 @@ tasks.test {
 }
 
 
-
-
 application {
     mainClass.set("dsdms.client.Main")
 }
+
+tasks.register<Exec>("myRun") {//inline function with reified type!
+    //Configuration action is of type T.() -> Unit, in this case Exec.T() -> Unit
+    val javaExecutable = org.gradle.internal.jvm.Jvm.current().javaExecutable.absolutePath
+    commandLine( // this is a method of class org.gradle.api.Exec
+        javaExecutable, "-version"
+    )
+    //There is no need of doLast / doFirst, actions are already configured
+    //Still, we may want to do something before or after the task has been executed
+    doLast { println("$javaExecutable invocation complete") }
+    doFirst { println("Ready to invoke $javaExecutable") }
+}
+
+
