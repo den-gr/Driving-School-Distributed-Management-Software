6183d2e5f8f55e5a8165b1941dedd0d14ff0368a
Denys <denys.grushchak@studio.unibo.it>
2023-04-17 21:26:23 +0200

    fix json serialization error

--- SystemTester/src/test/kotlin/client/cucumber/Dossier.kt
+++ SystemTester/src/test/kotlin/client/cucumber/Dossier.kt
@@ -4,18 +4,18 @@ import dsdms.client.SmartSleep
 import io.cucumber.java8.En
 import io.cucumber.junit.Cucumber
 import io.cucumber.junit.CucumberOptions
+import io.vertx.core.buffer.Buffer
 import io.vertx.core.json.JsonObject
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.junit.runner.RunWith
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
-fun giveJsonObject(name: String, surname: String, fiscal_code: String): JsonObject {
-    return JsonObject.of(
-        "name", name,
-        "surname", surname,
-        "fiscal_code", fiscal_code
-    )
-}
+@Serializable
+data class SubscriberDocuments(val name: String,
+                               val surname: String, val fiscal_code: String)
 
 @RunWith(Cucumber::class)
 @CucumberOptions(
@@ -29,8 +29,8 @@ class Dossier : En {
         val sleeper = SmartSleep()
         When("I send {word}, {word}, {word} to server") {name: String, surname: String, fiscal_code: String ->
             val request = client
-               .post(8000, "localhost", "/dossiers")
-                .sendJson(giveJsonObject(name, surname, fiscal_code))
+                .post(8000, "localhost", "/dossiers")
+                .sendBuffer(Buffer.buffer(Json.encodeToString(SubscriberDocuments(name, surname, fiscal_code))))
             val response = sleeper.waitResult(request)
 
             assertNotNull(response)
