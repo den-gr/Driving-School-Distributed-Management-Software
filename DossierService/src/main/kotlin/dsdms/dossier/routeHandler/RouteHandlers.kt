991dfea04bb106527a49bf6ae0db9bc1195e65f0
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-04-18 11:50:09 +0200

    Add route management for dossier's reading

--- DossierService/src/main/kotlin/dsdms/dossier/routeHandler/RouteHandlers.kt
+++ DossierService/src/main/kotlin/dsdms/dossier/routeHandler/RouteHandlers.kt
@@ -1,9 +1,9 @@
-package dsdms.dossier
+package dsdms.dossier.routeHandler
 
 import io.vertx.ext.web.RoutingContext
 
 interface RouteHandlers {
     fun handleDossierRegistration(routingContext: RoutingContext)
 
-    fun handleDossierReading(routingContext: RoutingContext)
+    fun handleDossierIdReading(routingContext: RoutingContext)
 }
