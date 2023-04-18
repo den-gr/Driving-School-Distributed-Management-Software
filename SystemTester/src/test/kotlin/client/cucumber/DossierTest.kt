2180e5682e23d4686d352c9deab9ef091e7d587e
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-04-18 11:48:56 +0200

    Add tests for duplication and error messages while creating dossiers

---
+++ SystemTester/src/test/kotlin/client/cucumber/DossierTest.kt
@@ -0,0 +1,93 @@
+package client.cucumber
+
+import client.utils.createJson
+import dsdms.client.SmartSleep
+import dsdms.dossier.serialization.Dossier
+import dsdms.dossier.serialization.SubscriberDocuments
+import io.cucumber.java8.En
+import io.cucumber.junit.Cucumber
+import io.cucumber.junit.CucumberOptions
+import io.vertx.core.json.JsonObject
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.junit.runner.RunWith
+import kotlin.test.assertEquals
+import java.net.HttpURLConnection.*
+import kotlin.test.assertNotNull
+
+@RunWith(Cucumber::class)
+@CucumberOptions(
+    features = ["src/test/resources/features/registerAndReadSubscriber.feature"],
+    plugin = ["pretty", "summary"]
+)
+class DossierTest : En {
+    private var value: Int = -1
+    private var retrievedDossier: Dossier? = null
+
+    init {
+        val sleeper = SmartSleep()
+        When("I send {word}, {word}, {word} to server") {name: String, surname: String, fiscal_code: String ->
+            val request = client
+                .post(8000, "localhost", "/dossiers")
+                .sendBuffer(createJson(SubscriberDocuments(name, surname, fiscal_code)))
+            val response = sleeper.waitResult(request)
+
+            assertNotNull(response)
+            value = response.body().toString().toInt()
+        }
+        Then("I received {int} of registered dossier") { id: Int ->
+            println("Value: $value")
+            assertEquals(id, value) // first dossier registered
+        }
+
+        When("I send {int} to server") { id: Int ->
+            val request = client
+                .get(8000, "localhost", "/dossiers/$id")
+                .send()
+            val response = sleeper.waitResult(request)
+            assertNotNull(response)
+
+            retrievedDossier = Json.decodeFromString(response.body().toString())
+        }
+
+        Then("I received {word}, {word}, {word} of registered dossier") {name: String, surname: String, fiscal_code: String ->
+            assertNotNull(retrievedDossier)
+            assertEquals(name, retrievedDossier?.name)
+            assertEquals(surname, retrievedDossier?.surname)
+            assertEquals(fiscal_code, retrievedDossier?.fiscal_code)
+        }
+
+        When("I send bad informations {word}, {int}, {word} to server") {name: String, surname: Int, fiscal_code: String ->
+            val request = client
+                .post(8000, "localhost", "/dossiers")
+                .sendJson(JsonObject.of(
+                    "name", name,
+                    "surname", surname,
+                    "fiscal_code", fiscal_code))
+            val response = sleeper.waitResult(request)
+
+            assertNotNull(response)
+            value = response.statusCode()
+        }
+
+        Then("I received Bad request error message") {
+            assertNotNull(value)
+            assertEquals(HTTP_BAD_REQUEST, value)
+        }
+
+        When("I send duplicated informations {word}, {word}, {word} to server") {name: String, surname: String, fiscal_code: String ->
+            val request = client
+                .post(8000, "localhost", "/dossiers")
+                .sendBuffer(createJson(SubscriberDocuments(name, surname, fiscal_code)))
+            val response = sleeper.waitResult(request)
+
+            assertNotNull(response)
+            value = response.statusCode()
+        }
+
+        Then("I received Conflict error message") {
+            assertNotNull(value)
+            assertEquals(HTTP_CONFLICT, value)
+        }
+    }
+}
--- SystemTester/src/test/resources/features/registerAndReadSubscriber.feature
+++ SystemTester/src/test/resources/features/registerAndReadSubscriber.feature
@@ -1,9 +1,32 @@
 Feature: registration and reading of new dossier
     @vertx
-    Scenario Outline: subscriber informations are correct
+    Scenario Outline: subscriber information's are correct
         When I send <name>, <surname>, <fiscal_code> to server
-        Then I received Id of registered dossier
+        Then I received <id> of registered dossier
 
     Examples: basic information
-    | name | surname | fiscal_code |
-    | Riccardo | Bacca | BCCRCR99C07C573X |
+        | id | name | surname | fiscal_code |
+        | 0 | Riccardo | Bacca | BCCRCR99C07C573X |
+
+    @vertx
+    Scenario Outline: subscriber information's are correct
+        When I send <id> to server
+        Then I received <name>, <surname>, <fiscal_code> of registered dossier
+
+    Examples: basic information
+        |id | name | surname | fiscal_code |
+        |0 | Riccardo | Bacca | BCCRCR99C07C573X |
+
+    @vertx
+    Scenario: subscriber information's are not correct
+        When I send bad informations b, 123, BCCRCR99C07C573X to server
+        Then I received Bad request error message
+
+    @vertx
+    Scenario Outline: subscriber informations are already available in a valid dossier
+        When I send duplicated informations <name>, <surname>, <fiscal_code> to server
+        Then I received Conflict error message
+
+    Examples: basic information
+        | name | surname | fiscal_code |
+        | Riccardo | Bacca | BCCRCR99C07C573X |
