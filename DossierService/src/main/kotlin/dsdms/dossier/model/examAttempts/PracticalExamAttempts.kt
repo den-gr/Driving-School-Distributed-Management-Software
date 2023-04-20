feae87b4d88c89c680a10eb03f3df028fa111ae0
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-04-20 16:15:13 +0200

    Add JsonDeserialize tag to deserialize interface

---
+++ DossierService/src/main/kotlin/dsdms/dossier/model/examAttempts/PracticalExamAttempts.kt
@@ -0,0 +1,13 @@
+package dsdms.dossier.model.examAttempts
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import kotlinx.serialization.Serializable
+
+@Serializable
+@JsonDeserialize(`as` = PracticalExamAttemptsImpl::class)
+sealed interface PracticalExamAttempts {
+
+    var attempts: Int
+
+    fun verifyAttempts(): Boolean
+}
