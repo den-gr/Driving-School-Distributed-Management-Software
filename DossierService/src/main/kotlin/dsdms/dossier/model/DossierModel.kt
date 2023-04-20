be497797f7aa71fe3e84d8d5d4dcc050765c57dc
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-04-20 16:16:03 +0200

    Add Id auto generating with Kmongo

--- DossierService/src/main/kotlin/dsdms/dossier/model/DossierModel.kt
+++ DossierService/src/main/kotlin/dsdms/dossier/model/DossierModel.kt
@@ -1,28 +1,38 @@
 package dsdms.dossier.model
 
-import dsdms.dossier.database.FakeDB
+import com.mongodb.client.MongoDatabase
+import dsdms.dossier.database.MongoDossier
 import dsdms.dossier.database.Repository
-import dsdms.dossier.model.exam.PracticalExamAttemptsImpl
+import dsdms.dossier.model.examAttempts.PracticalExamAttemptsImpl
+import dsdms.dossier.model.examStatus.ExamStatusImpl
 
 
-class DossierModel {
-    private val repository: Repository = FakeDB()
+class DossierModel(dossierServiceDb: MongoDatabase) {
+    private val newRepo: Repository = MongoDossier(dossierServiceDb)
     private fun validateNewDossier(newDossier: Dossier): Boolean {
-        val alreadyExistingDossiers: List<Dossier> = repository.readDossierFromCf(newDossier.fiscal_code)
-        return alreadyExistingDossiers.count { el -> el.validity } == 0
+//        val alreadyExistingDossiers: List<Dossier> = repository.readDossierFromCf(newDossier.fiscal_code)
+//        return alreadyExistingDossiers.count { el -> el.validity } == 0
+        return true
     }
 
-    fun saveNewDossier(documents: SubscriberDocuments): Int? {
+    fun saveNewDossier(documents: SubscriberDocuments): String? {
         val dossier = Dossier(documents.name, documents.surname, documents.fiscal_code,
-            true, null,
-            examAttempts = PracticalExamAttemptsImpl()
+            true,
+            examAttempts = PracticalExamAttemptsImpl(), examStatus = ExamStatusImpl()
         )
         return if (validateNewDossier(dossier)) {
-            repository.createDossier(dossier)
+            newRepo.createDossier(dossier)
         } else null
     }
 
-    fun readDossierFromId(id: Int): Dossier? {
-        return repository.readDossierFromId(id)
+    fun readDossierFromId(id: String): Dossier? {
+        return newRepo.readDossierFromId(id)
+    }
+
+    fun updateExamStatus(data: ExamStatusUpdate, id: Int) {
+//        return if (data.exam == "theoretical")
+//            this.repository.changeTheoreticalExamStatus(data.newStatus, id)
+//        else
+//            this.repository.changePracticalExamStatus(data.newStatus, id)
     }
 }
--- DossierService/src/main/kotlin/dsdms/dossier/model/Serializable.kt
+++ DossierService/src/main/kotlin/dsdms/dossier/model/Serializable.kt
@@ -1,6 +1,10 @@
 package dsdms.dossier.model
 
-import dsdms.dossier.model.exam.PracticalExamAttemptsImpl
+import dsdms.dossier.model.examAttempts.PracticalExamAttempts
+import dsdms.dossier.model.examAttempts.PracticalExamAttemptsImpl
+import dsdms.dossier.model.examStatus.ExamStatus
+import dsdms.dossier.model.examStatus.ExamStatusImpl
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 
 /**
@@ -10,10 +14,14 @@ import kotlinx.serialization.Serializable
 data class Dossier(
     val name: String,
     val surname: String,
-    val fiscal_code: String, val validity: Boolean,
-    val id: Int?, val examAttempts: PracticalExamAttemptsImpl
+    val fiscal_code: String, val validity: Boolean = true,
+    @Contextual val _id: String? = null, val examAttempts: PracticalExamAttempts = PracticalExamAttemptsImpl(), val examStatus: ExamStatus = ExamStatusImpl()
 )
 
 @Serializable
 data class SubscriberDocuments(val name: String,
                                val surname: String, val fiscal_code: String)
+
+@Serializable
+data class ExamStatusUpdate(val exam: String,
+                               val newStatus: Boolean)
