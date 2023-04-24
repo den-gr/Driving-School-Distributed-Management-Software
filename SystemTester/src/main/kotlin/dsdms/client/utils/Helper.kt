1327f258c572857298d9031a5ba7986b839af49e
Denys <denys.grushchak@studio.unibo.it>
2023-04-24 15:58:12 +0200

    minor fixes

--- SystemTester/src/main/kotlin/dsdms/client/utils/Helper.kt
+++ SystemTester/src/main/kotlin/dsdms/client/utils/Helper.kt
@@ -1,10 +1,20 @@
 package dsdms.client.utils
 
 import io.vertx.core.buffer.Buffer
+import io.vertx.ext.web.client.HttpResponse
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import org.junit.Assume
+import kotlin.test.assertNotNull
 
 
 inline fun<reified T> createJson(docs: T): Buffer? {
     return Buffer.buffer(Json.encodeToString(docs))
+}
+
+fun checkResponse(res: HttpResponse<Buffer>?){
+    assertNotNull(res)
+    Assume.assumeNotNull(res)
+    assertNotNull(res.body())
+    Assume.assumeNotNull(res.body())
 }
