1e8ae8ed5f84a6c75e5daf141a56b0c022d9b883
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-05-02 09:59:22 +0200

    Updated driving service build.gradle

--- DrivingService/build.gradle.kts
+++ DrivingService/build.gradle.kts
@@ -1,20 +1,48 @@
 group = "dsdms.driving"
 version = "0.0.1"
 
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
-//    alias(libs.plugins.vertx)
+    alias(libs.plugins.vertx)
     alias(libs.plugins.johnrengelman.shadow)
-//    application
+    alias(libs.plugins.kotest.multiplatform)
+    application
+
+    id("java-library")
 }
 
+vertx.mainVerticle="dsdms.driving.Main" //TODO
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation(kotlin("stdlib-jdk8"))
+    implementation(libs.bundles.kotlinx)
+    implementation(libs.bundles.kmongo)
+    implementation(libs.bundles.vertx.server)
+    implementation(libs.bundles.kotest)
 }
 
 repositories {
     mavenCentral()
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = "dsdms.driving.Main"
+    }
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+    destinationDirectory.set(file("$buildDir/output"))
+}
+
+application {
+    mainClass.set("dsdms.driving.Main")
 }
