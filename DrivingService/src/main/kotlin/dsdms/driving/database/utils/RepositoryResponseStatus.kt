c28d3475a0979ca3b3d850854be2bfa644bccd4f
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-05-03 16:07:30 +0200

    Add response status enums for driving service routes

---
+++ DrivingService/src/main/kotlin/dsdms/driving/database/utils/RepositoryResponseStatus.kt
@@ -0,0 +1,8 @@
+package dsdms.driving.database.utils
+
+enum class RepositoryResponseStatus {
+    OK,
+    DELETE_ERROR,
+    UPDATE_ERROR,
+    MULTIPLE_EQUAL_IDS
+}
---
+++ DrivingService/src/main/kotlin/dsdms/driving/model/domainServices/DomainResponseStatus.kt
@@ -0,0 +1,7 @@
+package dsdms.driving.model.domainServices
+
+enum class DomainResponseStatus {
+    OK,
+    FISCAL_CODE_DUPLICATION,
+    ID_NOT_FOUND
+}
