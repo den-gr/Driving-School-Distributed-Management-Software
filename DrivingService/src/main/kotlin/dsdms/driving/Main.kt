bea881c033048634d7d33043c3e1a64c6a2b2add
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-05-05 15:41:57 +0200

    Removed un-useful imports and tags

--- DrivingService/src/main/kotlin/dsdms/driving/Main.kt
+++ DrivingService/src/main/kotlin/dsdms/driving/Main.kt
@@ -1,13 +1,11 @@
 package dsdms.driving
 
 import io.vertx.core.Vertx
-import kotlinx.coroutines.DelicateCoroutinesApi
 import org.litote.kmongo.KMongo
 
 class Main {
     companion object{
         @JvmStatic
-        @DelicateCoroutinesApi
         fun main(args : Array<String>){
             val port = 8010
             println("Driving service started")
--- DrivingService/src/main/kotlin/dsdms/driving/Server.kt
+++ DrivingService/src/main/kotlin/dsdms/driving/Server.kt
@@ -6,26 +6,18 @@ import dsdms.driving.database.RepositoryImpl
 import dsdms.driving.handlers.RouteHandlers
 import dsdms.driving.handlers.RouteHandlersImpl
 import dsdms.driving.model.ModelImpl
-import io.vertx.core.Vertx
-import io.vertx.ext.web.Route
+import io.vertx.core.AbstractVerticle
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BodyHandler
-import io.vertx.kotlin.coroutines.CoroutineVerticle
-import io.vertx.kotlin.coroutines.dispatcher
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.launch
-import org.litote.kmongo.coroutine.CoroutineDatabase
 import kotlin.system.exitProcess
 
-@DelicateCoroutinesApi
-class Server(private val port: Int, dbConnection: MongoDatabase) : CoroutineVerticle() {
+class Server(private val port: Int, dbConnection: MongoDatabase) : AbstractVerticle() {
 
     private val repository: Repository = RepositoryImpl(dbConnection)
-
     private val handlersImpl: RouteHandlers = RouteHandlersImpl(ModelImpl(repository))
 
-    override suspend fun start() {
+    override fun start() {
         val router: Router = Router.router(vertx)
         router.route().handler(BodyHandler.create())
 
@@ -41,20 +33,20 @@ class Server(private val port: Int, dbConnection: MongoDatabase) : CoroutineVert
     }
 
     private fun setRoutes(router: Router) {
-        router.post("/drivingSlots").handler(handlersImpl::registerDrivingSlot)
+        router.get("/drivingSlots").handler(handlersImpl::getOccupiedDrivingSlots)
     }
 
-    private fun Route.coroutineHandler(fn: suspend (RoutingContext) -> Unit) {
-        handler { ctx ->
-            launch(Vertx.currentContext().dispatcher()) {
-                try {
-                    fn(ctx)
-                } catch (e: Exception) {
-                    ctx.fail(e)
-                }
-            }
-        }
-    }
+//    private fun Route.coroutineHandler(fn: suspend (RoutingContext) -> Unit) {
+//        handler { ctx ->
+//            launch(Vertx.currentContext().dispatcher()) {
+//                try {
+//                    fn(ctx)
+//                } catch (e: Exception) {
+//                    ctx.fail(e)
+//                }
+//            }
+//        }
+//    }
 
     private fun handle(routingContext: RoutingContext){
         val id = routingContext.request().getParam("id").toIntOrNull()
