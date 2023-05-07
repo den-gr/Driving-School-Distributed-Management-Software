0bfa4fffeb7e70ff4f6127ca96523f63e8921cef
Denys <denys.grushchak@studio.unibo.it>
2023-05-07 16:57:21 +0200

    use mongodb URI and DrivingService port as argument to the program

--- DrivingService/src/main/kotlin/dsdms/driving/Main.kt
+++ DrivingService/src/main/kotlin/dsdms/driving/Main.kt
@@ -5,13 +5,22 @@ import org.litote.kmongo.KMongo
 
 class Main {
     companion object{
+        private const val DEFAULT_MONGO_URI = "mongodb://admin:admin@localhost:27017"
+        private const val DEFAULT_SERVER_PORT = 8010
+
         @JvmStatic
         fun main(args : Array<String>){
-            val port = 8010
+            val port = if (System.getProperty("driving_port") != null) System.getProperty("driving_port").toInt()
+                else DEFAULT_SERVER_PORT
+
+            val mongoURI = if (System.getProperty("mongo_uri") != null) System.getProperty("mongo_uri")
+                else DEFAULT_MONGO_URI
+            println("Port: $port")
+            println("MongoURI: $mongoURI")
             println("Driving service started")
 
             val dbConnection = KMongo
-                .createClient("mongodb://admin:admin@mongo:27017")
+                .createClient(mongoURI)
                 .getDatabase("driving_service")
             val server = Server(port, dbConnection)
             Vertx.vertx().deployVerticle(server)
