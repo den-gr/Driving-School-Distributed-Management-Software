de7bcad41e8053d7e25e8feecd1040ed5e4e1cc4
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-05-11 12:04:37 +0200

    fix: add interface to dossier service

--- DossierService/src/main/kotlin/dsdms/dossier/model/domainServices/DossierService.kt
+++ DossierService/src/main/kotlin/dsdms/dossier/model/domainServices/DossierService.kt
@@ -1,44 +1,16 @@
 package dsdms.dossier.model.domainServices
 
-import dsdms.dossier.database.Repository
 import dsdms.dossier.database.utils.RepositoryResponseStatus
 import dsdms.dossier.model.entities.Dossier
 import dsdms.dossier.model.valueObjects.ExamStatusUpdate
 import dsdms.dossier.model.valueObjects.SubscriberDocuments
 
-class DossierService(private val repository: Repository) {
-    private suspend fun checkDuplicatedFiscalCode(givenDocuments: SubscriberDocuments): Boolean {
-        val alreadyExistingDossiers: List<Dossier> = repository.readDossierFromCf(givenDocuments.fiscal_code)
-        return alreadyExistingDossiers.count { el -> el.validity } == 0
-    }
+interface DossierService {
+    suspend fun saveNewDossier(givenDocuments: SubscriberDocuments): String?
 
-    suspend fun saveNewDossier(givenDocument: SubscriberDocuments): String? {
-        return repository.createDossier(Dossier(givenDocument.name, givenDocument.surname, givenDocument.fiscal_code))
-    }
+    suspend fun verifyDocuments(documents: SubscriberDocuments): DomainResponseStatus
 
-    suspend fun verifyDocuments(documents: SubscriberDocuments): DomainResponseStatus {
-        return if (checkDuplicatedFiscalCode(documents)) {
-            DomainResponseStatus.OK
-        } else {
-            DomainResponseStatus.FISCAL_CODE_DUPLICATION
-        }
-    }
+    suspend fun readDossierFromId(id: String): Dossier?
 
-    suspend fun readDossierFromId(id: String): Dossier? {
-        return repository.readDossierFromId(id)
-    }
-
-    suspend fun updateExamStatus(data: ExamStatusUpdate, id: String): RepositoryResponseStatus {
-        val newStatus = readDossierFromId(id)?.examStatus
-        if (data.exam == "theoretical") {
-            newStatus?.modifyTheoretical(data.newStatus)
-        } else {
-            newStatus?.modifyPractical(data.newStatus)
-        }
-        return repository.updateExamStatus(newStatus, id)
-    }
-
-    suspend fun deleteDossier(id: String): RepositoryResponseStatus {
-        return repository.deleteDossier(id)
-    }
+    suspend fun updateExamStatus(data: ExamStatusUpdate, id: String): RepositoryResponseStatus
 }
