01e263261e4e42fb4ae4ca71fad17efa3cdcd9c6
Denys <denys.grushchak@studio.unibo.it>
2023-05-14 20:46:57 +0200

    refactor(SystemTester): reduce duplication in VertxProvider and add examService Client

--- SystemTester/src/main/kotlin/dsdms/client/utils/VertxProvider.kt
+++ SystemTester/src/main/kotlin/dsdms/client/utils/VertxProvider.kt
@@ -9,6 +9,8 @@ interface VertxProvider {
     fun getDossierServiceClient(): WebClient
 
     fun getDrivingServiceClient(): WebClient
+
+    fun getExamServiceClient(): WebClient
 }
 
 class VertxProviderImpl : VertxProvider {
@@ -16,23 +18,31 @@ class VertxProviderImpl : VertxProvider {
         const val LOCALHOST: String = "localhost"
         const val DEFAULT_DOSSIER_SERVICE_PORT = 8000
         const val DEFAULT_DRIVING_SERVICE_PORT = 8010
+        const val DEFAULT_EXAM_SERVICE_PORT = 8020
     }
     private val vertx: Vertx = Vertx.vertx()
 
     override fun getDossierServiceClient(): WebClient {
-        val host = if (System.getProperty("dossier_host") != null) System.getProperty("dossier_host") else LOCALHOST
-        val port = if (System.getProperty("dossier_port") != null) System.getProperty("dossier_port").toInt() else DEFAULT_DOSSIER_SERVICE_PORT
-
-        val options: WebClientOptions = WebClientOptions()
-            .setDefaultPort(port)
-            .setDefaultHost(host)
-        return WebClient.create(vertx, options)
+        return createClient(getHost("dossier_host"), getPort("dossier_port", DEFAULT_DOSSIER_SERVICE_PORT))
     }
 
     override fun getDrivingServiceClient(): WebClient {
-        val host = if (System.getProperty("driving_host") != null) System.getProperty("driving_host") else LOCALHOST
-        val port = if (System.getProperty("driving_port") != null) System.getProperty("driving_port").toInt() else DEFAULT_DRIVING_SERVICE_PORT
+        return createClient(getHost("driving_host"), getPort("driving_port", DEFAULT_DRIVING_SERVICE_PORT ))
+    }
 
+    override fun getExamServiceClient(): WebClient {
+        return createClient(getHost("exam_host"), getPort("exam_port", DEFAULT_EXAM_SERVICE_PORT))
+    }
+
+    private fun getHost(hostName: String): String{
+        return if (System.getProperty(hostName) != null) System.getProperty(hostName) else LOCALHOST
+    }
+
+    private fun getPort(portName: String, defaultPort: Int): Int {
+        return  if (System.getProperty(portName) != null) System.getProperty(portName).toInt() else defaultPort
+    }
+
+    private fun createClient(host: String, port: Int): WebClient {
         val options: WebClientOptions = WebClientOptions()
             .setDefaultPort(port)
             .setDefaultHost(host)
