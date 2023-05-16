3c7aff417cf26d93ffc10ba3f0191390d7d0cb42
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-05-16 14:56:42 +0200

    feat(DoctorService): add service model implementation for insert, get and delete of doctor slots

--- DoctorService/src/main/kotlin/dsdms/doctor/model/domainServices/DoctorService.kt
+++ DoctorService/src/main/kotlin/dsdms/doctor/model/domainServices/DoctorService.kt
@@ -1,3 +1,29 @@
 package dsdms.doctor.model.domainServices
 
-interface DoctorService {}
+import dsdms.doctor.model.entities.DoctorSlot
+import dsdms.doctor.model.valueObjects.GetBookedDoctorSlots
+
+interface DoctorService {
+
+    /**
+     * Verify that wanted date is a DoctorDay, wanted time is between doctor init
+     * and finish time and that in the wanted day, the wanted time is available
+     * @return specific domain response status for each error case
+     */
+    fun verifyDocuments(documents: DoctorSlot): DomainResponseStatus
+
+    /**
+     * @return the date in which the doctor slot has been put
+     */
+    fun saveDoctorSlot(documents: DoctorSlot): String
+
+    /**
+     * @return list of possible occupied doctor slots in a provided date
+     */
+    fun getOccupiedDoctorSlots(data: GetBookedDoctorSlots): List<DoctorSlot>
+
+    /**
+     * @return Domain response status: OK if delete was successful, DELETE_ERROR otherwise
+     */
+    fun deleteDoctorSlot(dossierId: String): DomainResponseStatus
+}
--- DoctorService/src/main/kotlin/dsdms/doctor/model/domainServices/DoctorServiceImpl.kt
+++ DoctorService/src/main/kotlin/dsdms/doctor/model/domainServices/DoctorServiceImpl.kt
@@ -1,5 +1,38 @@
 package dsdms.doctor.model.domainServices
 
 import dsdms.doctor.database.Repository
+import dsdms.doctor.handlers.getDomainCode
+import dsdms.doctor.handlers.repositoryToDomainConversionTable
+import dsdms.doctor.model.entities.DoctorDays
+import dsdms.doctor.model.entities.DoctorSlot
+import dsdms.doctor.model.entities.DoctorTimeSlot
+import dsdms.doctor.model.valueObjects.GetBookedDoctorSlots
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.toLocalTime
 
-class DoctorServiceImpl(private val repository: Repository) : DoctorService {}
+class DoctorServiceImpl(private val repository: Repository) : DoctorService {
+
+    override fun verifyDocuments(documents: DoctorSlot): DomainResponseStatus {
+        return if (DoctorDays.values().any { el -> el.name == LocalDate.parse(documents.date).dayOfWeek.name}.not())
+            DomainResponseStatus.NOT_DOCTOR_DAY
+        else if ((documents.time.toLocalTime() > DoctorTimeSlot.InitTime.time && documents.time.toLocalTime() < DoctorTimeSlot.FinishTime.time).not())
+            DomainResponseStatus.BAD_TIME
+        else if (getOccupiedDoctorSlots(GetBookedDoctorSlots(documents.date)).any { el -> el.time == documents.time })
+            DomainResponseStatus.TIME_OCCUPIED
+        else if (getOccupiedDoctorSlots(GetBookedDoctorSlots(documents.date)).any { el -> el.dossierId == documents.dossierId })
+            DomainResponseStatus.DOSSIER_ALREADY_BOOKED
+        else DomainResponseStatus.OK
+    }
+
+    override fun saveDoctorSlot(documents: DoctorSlot): String {
+        return repository.saveDoctorSlot(documents)
+    }
+
+    override fun getOccupiedDoctorSlots(data: GetBookedDoctorSlots): List<DoctorSlot> {
+        return repository.getOccupiedDoctorSlots(data)
+    }
+
+    override fun deleteDoctorSlot(dossierId: String): DomainResponseStatus {
+        return repositoryToDomainConversionTable.getDomainCode(repository.deleteDoctorSlot(dossierId))
+    }
+}
