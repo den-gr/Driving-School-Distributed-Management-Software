684bab7e6452d06a4abc9c0a5a54938ddfef326e
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-05-19 09:40:04 +0200

    fix(DoctorService): vertx instance passed from doctorService server class

--- DoctorService/src/main/kotlin/dsdms/doctor/model/domainServices/vertxClient/VertxClientProvider.kt
+++ DoctorService/src/main/kotlin/dsdms/doctor/model/domainServices/vertxClient/VertxClientProvider.kt
@@ -4,13 +4,11 @@ import io.vertx.core.Vertx
 import io.vertx.ext.web.client.WebClient
 import io.vertx.ext.web.client.WebClientOptions
 
-class VertxClientProvider: VertxClient {
+class VertxClientProvider(private val vertx: Vertx): VertxClient {
     companion object {
         const val LOCALHOST: String = "localhost"
         const val DEFAULT_DOSSIER_SERVICE_PORT = 8000
     }
-
-    private val vertx: Vertx = Vertx.vertx()
 
     override suspend fun getDossierServiceClient(): WebClient {
         return createClient(getHost("dossier_host"), getPort("dossier_port", DEFAULT_DOSSIER_SERVICE_PORT))
