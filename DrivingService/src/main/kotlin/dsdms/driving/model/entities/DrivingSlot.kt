eaddb0877eb74713aca853991888414b48481a48
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-05-22 09:42:53 +0200

    fix(DrivingService): problem with previous commit that has been reverted

--- DrivingService/src/main/kotlin/dsdms/driving/model/entities/DrivingSlot.kt
+++ DrivingService/src/main/kotlin/dsdms/driving/model/entities/DrivingSlot.kt
@@ -5,12 +5,6 @@ import dsdms.driving.model.valueObjects.licensePlate.LicensePlate
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 
-/**
- * Complete set of info about a single Driving slot
- * @see LicensePlate
- * @see DrivingSlotType
- * @param _id can be used to access a single driving slot
- */
 @Serializable
 data class DrivingSlot(
     val date: String,
--- DrivingService/src/main/kotlin/dsdms/driving/model/valueObjects/licensePlate/LicensePlate.kt
+++ DrivingService/src/main/kotlin/dsdms/driving/model/valueObjects/licensePlate/LicensePlate.kt
@@ -3,17 +3,8 @@ package dsdms.driving.model.valueObjects.licensePlate
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import kotlinx.serialization.Serializable
 
-/**
- * Representing a singular license plate
- * A license plate identifies a single vehicle
- */
 @Serializable
 @JsonDeserialize(`as` = LicensePlateInit::class)
 sealed interface LicensePlate {
-
-    /**
-     * @param numberPlate: the new number plate to be verified
-     * @return true if the given string respects a specific Regex
-     */
     fun verifyStructure(numberPlate: String): Boolean
 }
