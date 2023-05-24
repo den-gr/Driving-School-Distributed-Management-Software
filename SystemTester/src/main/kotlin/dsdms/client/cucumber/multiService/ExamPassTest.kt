7cfa0bccd17895a90ba99ff58d9d7591e5bb2135
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-05-24 15:04:23 +0200

    feat(SystemTester): completed tests for theoretical exam pass creation and getting

---
+++ SystemTester/src/main/kotlin/dsdms/client/cucumber/multiService/ExamPassTest.kt
@@ -0,0 +1,86 @@
+package dsdms.client.cucumber.multiService
+import dsdms.client.utils.SmartSleep
+import dsdms.client.utils.VertxProviderImpl
+import dsdms.client.utils.checkResponse
+import dsdms.client.utils.createJson
+import dsdms.doctor.model.valueObjects.DoctorResult
+import dsdms.exam.model.entities.theoreticalExam.TheoreticalExamPass
+import io.cucumber.java8.En
+import io.cucumber.junit.Cucumber
+import io.cucumber.junit.CucumberOptions
+import io.vertx.ext.web.client.WebClient
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.junit.runner.RunWith
+import java.net.HttpURLConnection.HTTP_OK
+import kotlin.test.assertEquals
+
+@RunWith(Cucumber::class)
+@CucumberOptions(
+    features = ["src/main/resources/features/multiService/theoreticalExamPassTest.feature"],
+    plugin = ["pretty", "summary"]
+)
+class ExamPassTest : En {
+    private val doctorService: WebClient = VertxProviderImpl().getDoctorServiceClient()
+    private val examService: WebClient = VertxProviderImpl().getExamServiceClient()
+    private var value: String = ""
+    private var retrievedTheoreticalExamPass: TheoreticalExamPass? = null
+    private var statusCode: Int? = null
+
+    init {
+        val sleeper = SmartSleep()
+
+        When("secretary register {word}, {word}, {word}") { id: String, date: String, result: String ->
+            val request = doctorService
+                .put("/doctorSlots")
+                .sendBuffer(createJson(DoctorResult(id, date, result)))
+            val response = sleeper.waitResult(request)
+
+            checkResponse(response)
+            statusCode = response?.statusCode()
+            value = response?.body().toString()
+        }
+        Then("request finished with success, with code {int}") { code: Int ->
+            assertEquals(HTTP_OK, code)
+        }
+        And("theoretical exam pass is registered for dossier {word}") { dossierId: String ->
+            val request = examService
+                .get("/theoreticalExam/pass/$dossierId")
+                .send()
+            val response = sleeper.waitResult(request)
+
+            checkResponse(response)
+            assertEquals(HTTP_OK, response?.statusCode())
+
+            retrievedTheoreticalExamPass = Json.decodeFromString(response?.body().toString())
+        }
+        When("sending {word}, {word} and {word} for saving doctor results") { id: String, date: String, result: String ->
+            val request = doctorService
+                .put("/doctorSlots")
+                .sendBuffer(createJson(DoctorResult(id, date, result)))
+            val response = sleeper.waitResult(request)
+
+            checkResponse(response)
+            statusCode = response?.statusCode()
+            value = response?.body().toString()
+        }
+        Then("request finished with not success, with code {int} and message {word}") { code: Int, doctorMessage: String ->
+            assertEquals(code, statusCode)
+            assertEquals(doctorMessage, value)
+        }
+        And("theoretical exam pass is not being created for {word}, receiving message {word} and code {int}") { id: String, message: String, code: Int ->
+            val request = examService
+                .get("/theoreticalExam/pass/$id")
+                .send()
+            val response = sleeper.waitResult(request)
+
+            checkResponse(response)
+            assertEquals(code, response?.statusCode())
+
+            // Cause if status code is 200, exam service returns the founded theoretical exam pass for D1
+            // previously created
+            if (response?.statusCode() != HTTP_OK)
+                assertEquals(message, response?.body().toString())
+        }
+    }
+}
--- SystemTester/src/main/resources/features/multiService/theoreticalExamPassTest.feature
+++ SystemTester/src/main/resources/features/multiService/theoreticalExamPassTest.feature
@@ -1,19 +1,20 @@
 Feature: creation and reading of theoretical exam passes
   Scenario Outline: register one valid doctor result
-    When secretary register <id>, <date> and <visit_result>
+    When secretary register <id>, <date>, <result>
     Then request finished with success, with code <code>
-    And theoretical exam pass is registered
+    And theoretical exam pass is registered for dossier <id>
 
     Examples:
-      | id | date | visit_result | code |
-      | d1 | 2023-09-19 | VALID | 200 |
+      | id | date | code | result |
+      | d1 | 2023-09-19 | 200 | VALID |
 
   Scenario Outline: booking two doctor visits
     When sending <id>, <date> and <visit_result> for saving doctor results
-    Then request finished with not success, with code <code>
-    And theoretical exam pass is not being created, receiving message <message>
+    Then request finished with not success, with code <doctor_code> and message <message>
+    And theoretical exam pass is not being created for <id>, receiving message <exam_message> and code <exam_code>
 
     Examples:
-      | id | date | visit_result | code | message |
-      | d2 | 2023-09-19 | NOT_VALID | 200 | EXAM_PASS_NOT_CREATED |
-      | d1 | 2023-09-20 | VALID | 400 | EXAM_PASS_ALREADY_AVAILABLE |
+      | id | date | visit_result | doctor_code | message | exam_message | exam_code |
+      | d2 | 2023-09-19 | NOT_VALID | 400 | EXAM_PASS_NOT_CREATED | ID_NOT_FOUND | 404 |
+      | d3 | 2023-09-20 | NEED_ONE_MORE_VISIT | 400 | EXAM_PASS_NOT_CREATED | ID_NOT_FOUND | 404 |
+      | d1 | 2023-09-20 | VALID | 400 | EXAM_PASS_ALREADY_AVAILABLE | NULL | 200 |
