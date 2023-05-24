8c3cf8bb190521c16a5e11803fa5408420636c35
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2023-05-24 15:00:42 +0200

    feat(ExamService): add multiple error cases for creation and getting of theoretical exam pass

--- ExamService/src/main/kotlin/dsdms/exam/handlers/StatusConversionTable.kt
+++ ExamService/src/main/kotlin/dsdms/exam/handlers/StatusConversionTable.kt
@@ -9,20 +9,13 @@ import java.net.HttpURLConnection
 val repositoryToDomainConversionTable: Map<RepositoryResponseStatus, DomainResponseStatus> = mapOf(
     RepositoryResponseStatus.OK to DomainResponseStatus.OK,
     RepositoryResponseStatus.DELETE_ERROR to DomainResponseStatus.DELETE_ERROR,
-    RepositoryResponseStatus.NO_PROVISIONAL_LICENSE to DomainResponseStatus.NO_PROVISIONAL_LICENSE,
-    RepositoryResponseStatus.INVALID_PROVISIONAL_LICENSE to DomainResponseStatus.INVALID_PROVISIONAL_LICENSE
 )
 
 val domainConversionTable: Map<DomainResponseStatus, Int> = mapOf(
     DomainResponseStatus.OK to HttpURLConnection.HTTP_OK,
-    DomainResponseStatus.NO_SLOT_OCCUPIED to HttpURLConnection.HTTP_OK,
-    DomainResponseStatus.INSTRUCTOR_NOT_FREE to HttpURLConnection.HTTP_BAD_REQUEST,
-    DomainResponseStatus.VEHICLE_NOT_FREE to HttpURLConnection.HTTP_BAD_REQUEST,
-    DomainResponseStatus.OCCUPIED_DRIVING_SLOTS to HttpURLConnection.HTTP_BAD_REQUEST,
-    DomainResponseStatus.INVALID_PROVISIONAL_LICENSE to HttpURLConnection.HTTP_BAD_REQUEST,
-    DomainResponseStatus.NO_PROVISIONAL_LICENSE to HttpURLConnection.HTTP_BAD_REQUEST,
+    DomainResponseStatus.EXAM_PASS_ALREADY_AVAILABLE to HttpURLConnection.HTTP_BAD_REQUEST,
     DomainResponseStatus.DELETE_ERROR to HttpURLConnection.HTTP_BAD_REQUEST,
-    DomainResponseStatus.BAD_VEHICLE_INSTRUCTOR_INFO to HttpURLConnection.HTTP_NOT_FOUND,
+    DomainResponseStatus.ID_NOT_FOUND to HttpURLConnection.HTTP_NOT_FOUND
 )
 
 fun  Map<DomainResponseStatus, Int>.getHttpCode(domainResponseStatus: DomainResponseStatus): Int {
