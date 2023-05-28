e1e593295a3983e3fc5c0df102a2c068d045cf4d
Denys <denys.grushchak@studio.unibo.it>
2023-05-28 19:41:53 +0200

    ci: add automatic swagger release

--- release.config.js
+++ release.config.js
@@ -3,6 +3,7 @@ var publishCmd = `
 ./gradlew createJavadoc -Pversion=\${nextRelease.version} --parallel || exit 2
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 3
 git push --force origin \${nextRelease.version} || exit 4
+echo ::set-output name=nextVer::\${nextRelease.version} || exit 5
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
@@ -11,6 +12,10 @@ config.plugins.push(
     }],
     ["@semantic-release/github", {
         "assets": [
+            { "path": "DoctorService/build/libs/*.jar"},
+            { "path": "DoctorService/build/output/*.jar"},
+            { "path": "ExamService/build/libs/*.jar"},
+            { "path": "ExamService/build/output/*.jar"},
             { "path": "DossierService/build/libs/*.jar"},
             { "path": "DossierService/build/output/*.jar"},
             { "path": "DrivingService/build/libs/*.jar"},
